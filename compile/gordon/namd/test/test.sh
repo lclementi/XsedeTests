d0cf12017a07aff73add35c13e23e6141d21cc25
clem <luca.clementi@gmail.com>
2012-09-10 18:45:52 -0700

    Namd tests

---
+++ compile/gordon/namd/test/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#PBS -q normal
+#PBS -l nodes=gcn-4-73
+###PBS -l nodes=1:ppn=8:native
+#PBS -l walltime=00:03:00
+#PBS -N testFingerPrintCompute
+#PBS -o output
+#PBS -e error
+#PBS -A sds138
+#PBS -M clem@sdsc.edu
+#PBS -m abe
+#PBS -V
+# Start of user commands - comments start with a hash sign (#)
+
+echo Runing on `hostname`
+date
+email="clem@sdsc.edu"
+
+
+
+cd /oasis/scratch/clem/temp_project/XsedeTests/compile/gordon
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/namd/fftwbin/lib/
+
+
+cd tiny
+mpirun_rsh -np 8 -hostfile $PBS_NODEFILE ./../namd/bin/namd2 tiny.namd
+
+
