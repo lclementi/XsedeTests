340dcef8d375776d64d757f8ebbd737a87dfbb0b
clem <luca.clementi@gmail.com>
2012-09-10 19:07:55 -0700

    First try with the test of apbs
    Signed-off-by: clem <luca.clementi@gmail.com>

---
+++ compile/gordon/apbs/test/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#PBS -q normal
+#PBS -l nodes=gcn-4-73
+###PBS -l nodes=1:ppn=8:native
+#PBS -l walltime=00:03:00
+#PBS -N testFingerPrintCompute
+#PBS -o output
+#PBS -e error
+#PBS -A sds138
+#PBS -M clem@sdsc.edu
+#PBS -m abe
+#PBS -V
+# Start of user commands - comments start with a hash sign (#)
+
+echo Runing on `hostname`
+date
+
+
+echo The working directory is $PBS_O_WORKDIR 
+
+cd $PBS_O_WORKDIR 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+
+
+mpirun_rsh -np 8 -hostfile $PBS_NODEFILE ../bin/bin/apbs apbs-mol.in > apbs-mol.out
+
+
