afc3e43d3b94aeb89cbf9b2fdea8bd814bc85385
Luca Clementi <luca.clementi@gmail.com>
2012-09-25 23:24:58 +0000

    email notification on OSG and disabled integrity check

--- compile/osg/submit_condor.sh
+++ compile/osg/submit_condor.sh
@@ -18,20 +18,12 @@ function failure(){
     elif [ "$1" == "i" ];then
         echo -n " integrity " >> tmplog 
     fi
-    echo >>tmplog
+    echo $2 >>tmplog
     echo Hostname `hostname` >>tmplog 
     #cat error >> tmplog
     echo >> tmplog
-    cat tmplog | mail -s "error `hostname`" $email
-    #cat tmplog >> log
-    rm tmplog
-    #rm error
 }
 
-echo ls
-ls -rlta 
-echo
-echo
 
 export PATH=/usr/sbin:$PATH:FingerPrint/bin
 
@@ -42,8 +34,8 @@ FILELOG=`hostname`.`date +%m%d%H%M%Y`.log
 for i in $apps; do
     echo Verify $i >> $FILELOG
     fingerprint -y -v -s $i.csv -f $i.swirl >> $FILELOG || failure y $i
-    echo Integrity $i >> $FILELOG
-    fingerprint -i -v -s $i.csv -f $i.swirl >> $FILELOG || failure i $i
+    #echo Integrity $i >> $FILELOG
+    #fingerprint -i -v -s $i.csv -f $i.swirl >> $FILELOG || failure i $i
 done
 
 
