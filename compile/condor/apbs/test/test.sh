d586143088428626907a6e6fb3c4f35eb97d5516
Luca Clementi <luca.clementi@gmail.com>
2012-09-26 18:47:18 -0400

    fixed test of apbs with a more general approach

--- compile/condor/apbs/test/test.sh
+++ compile/condor/apbs/test/test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 module load mpich2/1.4.1p1_intel-12.1 
+
+rm hostfile
+for i in `seq 1 8`;do 
+    echo `hostname` >> hostfile ;
+done
+
 mpirun -np 8 -hostfile hostfile ../bin/bin/apbs apbs-mol-parallel.in
 
