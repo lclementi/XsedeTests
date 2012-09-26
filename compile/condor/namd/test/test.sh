de9109038c084b00e9260cfcfa225e3bb45692dd
Luca Clementi <luca.clementi@gmail.com>
2012-09-26 18:49:51 -0400

    more general script for namd testing

--- compile/condor/namd/test/test.sh
+++ compile/condor/namd/test/test.sh
@@ -6,6 +6,10 @@ email="clem@sdsc.edu"
 
 module load mpich2/1.4.1p1_intel-12.1
 
+rm hostfile
+for i in `seq 1 8`;do 
+    echo `hostname` >> hostfile ;
+done
 
 cd ../tiny
 mpirun -np 8 -hostfile ../test/hostfile ../bin/namd2 tiny.namd 
